ece98827bc69e6c8a1496c623ac8f5d908910840
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-10-20 11:51:56 +0500

    Banner UI Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,34 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <Head>
         <title>Medium Clone</title>
       </Head>
 
       <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0 ">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is a place to write, read & connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with million of readers
+          </h2>
+        </div>
+
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="http://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
+        <div></div>
+      </div>
     </div>
   );
 };
